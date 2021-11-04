d46ca0e39e6347a74f3f96e25cc2695c63dede3f
mrkeenan787 <mrkeenan@rams.colostate.edu>
2021-11-04 17:44:37 -0600

    10.2 11/4 POST /contacts

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const db = require("./dbConnectExec.js");
 
 const app = express();
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log(`app is running on port 5000`);
@@ -18,6 +20,51 @@ app.get("/", (req, res) => {
 
 // app.post();
 // app.put();
+
+app.post("/contacts", async (req, res) => {
+  //   res.send("/contacts called");
+
+  //   console.log("request body", req.body);
+
+  let nameFirst = req.body.nameFirst;
+  let nameLast = req.body.nameLast;
+  let email = req.body.email;
+  let address = req.body.address;
+  let password = req.body.password;
+
+  if (!nameFirst || !nameLast || !email || !address || !password) {
+    return res.status(400).send("Bad Request");
+  }
+
+  nameFirst = nameFirst.replace("'", "''");
+  nameLast = nameLast.replace("'", "''");
+
+  let emailCheckQuery = `SELECT Email 
+    FROM Customer
+    WHERE Email = '${email}'`;
+
+  let existingUser = await db.executeQuery(emailCheckQuery);
+
+  //   console.log("existing user", existingUser);
+
+  if (existingUser[0]) {
+    return res.status(409).send("Duplicate email");
+  }
+
+  let hashedPassword = bcrypt.hashSync(password);
+
+  let insertQuery = `INSERT INTO Customer(FirstName, LastName, Email, Address, Password)
+VALUES ('${nameFirst}' , '${nameLast}', '${email}', '${address}', '${hashedPassword}')`;
+
+  db.executeQuery(insertQuery)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log("error in POST /contact", err);
+      res.status(500).send();
+    });
+});
 
 app.get("/skis", (req, res) => {
   db.executeQuery(
