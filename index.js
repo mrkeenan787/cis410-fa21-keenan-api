d9d75bcb186d2f527dcc5cc6759493c6ab1a7e24
mrkeenan787 <mrkeenan@rams.colostate.edu>
2021-11-04 16:17:03 -0600

    10.1 10/26 /skis/:pk

--- index.js
+++ index.js
@@ -34,3 +34,27 @@ app.get("/skis", (req, res) => {
       res.status(500).send();
     });
 });
+
+app.get("/skis/:pk", (req, res) => {
+  let pk = req.params.pk;
+  //   console.log(pk);
+  let myQuery = `SELECT * 
+FROM Ski
+LEFT JOIN Category
+ON Category.CategoryPK = ski.CategoryFK
+WHERE SkiPK = ${pk}`;
+
+  db.executeQuery(myQuery)
+    .then((result) => {
+      // console.log("result", result);
+      if (result[0]) {
+        res.send(result[0]);
+      } else {
+        res.status(404).send(`bad request`);
+      }
+    })
+    .catch((err) => {
+      console.log("Error in /skis/:pk", err);
+      res.status(500).send();
+    });
+});
